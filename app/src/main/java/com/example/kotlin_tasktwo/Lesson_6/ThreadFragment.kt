723cd93190dcf8c0f311e7d284dc1bbfc5ec3b0d
iliy <gromov218974@mail.ru>
2022-04-19 19:43:54 +0400

    6-й урок. Создал новый поток MyThread. Добавил кнопку и тексты. И через метод GeneralText вывел поток.

--- app/src/main/java/com/example/kotlin_tasktwo/Lesson_6/ThreadFragment.kt
+++ app/src/main/java/com/example/kotlin_tasktwo/Lesson_6/ThreadFragment.kt
@@ -1,12 +1,14 @@
 package com.example.kotlin_tasktwo.Lesson_6
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.kotlin_tasktwo.databinding.TheardFragmentBinding
-import com.example.kotlin_tasktwo.view.main.MainActivity
 import java.lang.Thread.sleep
 
 
@@ -42,24 +44,62 @@ class ThreadFragment : Fragment() {
     @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super. onViewCreated(view,savedInstanceState)
+        val myThread = MyThread()
+            myThread.start()
         with(binding){
-            BtnDZ.setOnClickListener {
+            val fors = edTeFast.text.toString().toLong()
+            var num = 1
+            BtnFast.setOnClickListener {
                 Thread{
-                    val sek = editText.text.toString().toLong()
-                    sleep(sek * 1000L)
-                    requireActivity().runOnUiThread{TextView.text = " ${sek} секунды "}
-
+                    sleep(fors * 1000L)
+                    requireActivity().runOnUiThread{
+                        TextViewTwo.text = " ${fors} секунды "
+                        GeneralText("${ Thread.currentThread().name} ${num++}")
+                    }
                 }.start()
 
             }
+
+          BtnLong.setOnClickListener{
+              val long = edTeLong.text.toString().toLong()
+              myThread.mHandler?.post {
+                    sleep(long * 1000L)
+                    Handler(Looper.getMainLooper()).post{
+                        TextViewOne.text = " ${long} секунды "
+                        GeneralText("${ Thread.currentThread().name} ${num++}")
+                    }
+
+                }
+                }
+
+
+
+        }
+
+
         }
 
 
 
+
+private fun GeneralText(name:String){
+    binding.mainContainer.addView(TextView(requireContext()).apply {
+        text = name
+        textSize = 20f
+    })
+}
+
+
+    class MyThread:Thread(){
+            var mHandler:Handler? = null
+           override fun run() {
+               Looper.prepare()
+               mHandler = Handler(Looper.myLooper()!!)
+               Looper.loop()
+
+           }
+
     }
-
-
-
 
 
     companion object {
@@ -67,9 +107,9 @@ class ThreadFragment : Fragment() {
     }
 
 
-
-
 }
+
+
 
 
 
